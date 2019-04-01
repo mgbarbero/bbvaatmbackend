bea1af54386a8164d81791a0a8f5582393ba71ef
mgbarbero <mgbarbero@gmail.com>
2019-03-31 22:19:58 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -19,7 +19,7 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 class Info(Resource):
     description = {
         'id': 'bbvaatmmap-py',
-        'displayName': 'BBVA ATM MAP (PYTHON)',
+        'displayName': 'BBVA ATM MAP',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
         'zoom': 4
